4742fa94ad78e7cdf22d3f36ea98a2fe4227f3d0
Zack Allen <zack.m.allen@gmail.com>
2022-03-31 14:55:15 -0700

    Copyright and further cleanup

--- samples_project/Assets/SampleViewer/SampleSwitcher.cs
+++ samples_project/Assets/SampleViewer/SampleSwitcher.cs
@@ -1,3 +1,12 @@
+// Copyright 2021 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+// language governing permissions and limitations under the License.
+
 using Esri.HPFramework;
 using System.Collections.Generic;
 using System.IO;
@@ -33,20 +42,22 @@ public class SampleSwitcher : MonoBehaviour
     private void PopulateSampleSceneList()
     {
         SceneDropdown.options.Clear();
+
+        // Make a list of the formal names of the samples.
         var ApplicationPath = Application.dataPath;
         var SamplePath = ApplicationPath + "/SampleViewer/Samples/";
         List<string> SceneList = new List<string>();
         if (Directory.Exists(SamplePath))
         {
-            var Scenes = Directory.EnumerateFiles(SamplePath, "*.unity", SearchOption.AllDirectories);
-            foreach (string CurrentFile in Scenes)
+            var SampleScenePaths = Directory.EnumerateFiles(SamplePath, "*.unity", SearchOption.AllDirectories);
+            foreach (string CurrentFile in SampleScenePaths)
             {
-                string FileName = Path.GetFileNameWithoutExtension(CurrentFile);
-                SceneList.Add(FileName);
+                string SceneName = Path.GetFileNameWithoutExtension(CurrentFile);
+                SceneList.Add(SceneName);
             }
         }
+
         SceneDropdown.AddOptions(SceneList);
-        AddScene();
     }
 
     private void AddScene()
