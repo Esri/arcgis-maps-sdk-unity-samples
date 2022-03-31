5e54f7d64f3986f83e6de45111afe0bb95545ef9
Zack Allen <zack.m.allen@gmail.com>
2022-03-31 14:42:05 -0700

    Updated switcher to have more flexible scene load
    Ran codemaid on this too

--- samples_project/Assets/SampleViewer/SampleSwitcher.cs
+++ samples_project/Assets/SampleViewer/SampleSwitcher.cs
@@ -1,103 +1,102 @@
-using System.IO;
-using System.Collections;
+using Esri.HPFramework;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.Rendering;
-using UnityEngine.Events;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using Esri.HPFramework;
+using UnityEngine.UI;
 
 public class SampleSwitcher : MonoBehaviour
 {
-	public Dropdown PipelineTypeDropdown;
-	public Dropdown SceneDropdown;
-	public GameObject LightingObject;
-	private string PipelineType;
-	private string SceneName;
+    public Dropdown PipelineTypeDropdown;
+    public Dropdown SceneDropdown;
+    public GameObject LightingObject;
+    private string PipelineType;
+    private string SceneName;
 
-
-	void Start()
+    private void Start()
     {
-		SceneDropdown.onValueChanged.AddListener(delegate {
-			SceneChanged();
-		});
+        SceneDropdown.onValueChanged.AddListener(delegate
+        {
+            SceneChanged();
+        });
 
-		PipelineTypeDropdown.onValueChanged.AddListener(delegate {
-			PipelineChanged();
-		});
-		PipelineType = PipelineTypeDropdown.options[PipelineTypeDropdown.value].text;
+        PipelineTypeDropdown.onValueChanged.AddListener(delegate
+        {
+            PipelineChanged();
+        });
+        PipelineType = PipelineTypeDropdown.options[PipelineTypeDropdown.value].text;
 
-		PopulateSampleSceneList();
-	}
+        PopulateSampleSceneList();
+    }
 
-	private void PopulateSampleSceneList()
-	{
-		SceneDropdown.options.Clear();
-		var ApplicationPath = Application.dataPath;
-		var SamplePath = ApplicationPath + "/SampleViewer/Resources/SampleScenes/";
-		List<string> SceneList = new List<string>();
-		if (Directory.Exists(SamplePath))
-		{
-			var Scenes = Directory.EnumerateFiles(SamplePath, "*.unity");
-			foreach (string CurrentFile in Scenes)
-			{
-				string FileName = CurrentFile.Substring(SamplePath.Length, CurrentFile.Length - (".unity").Length - SamplePath.Length);
-				SceneList.Add(FileName);
-			}
-		}
-		SceneDropdown.AddOptions(SceneList);
-		AddScene();
-	}
+    private void PopulateSampleSceneList()
+    {
+        SceneDropdown.options.Clear();
+        var ApplicationPath = Application.dataPath;
+        var SamplePath = ApplicationPath + "/SampleViewer/Samples/";
+        List<string> SceneList = new List<string>();
+        if (Directory.Exists(SamplePath))
+        {
+            var Scenes = Directory.EnumerateFiles(SamplePath, "*.unity", SearchOption.AllDirectories);
+            foreach (string CurrentFile in Scenes)
+            {
+                string FileName = Path.GetFileNameWithoutExtension(CurrentFile);
+                SceneList.Add(FileName);
+            }
+        }
+        SceneDropdown.AddOptions(SceneList);
+        AddScene();
+    }
 
-	private void AddScene()
-	{
-		SceneName = SceneDropdown.options[SceneDropdown.value].text;
-		//The scene must also be added to the build settings list of scenes
-		SceneManager.LoadSceneAsync(SceneName, new LoadSceneParameters(LoadSceneMode.Additive));
-	}
+    private void AddScene()
+    {
+        SceneName = SceneDropdown.options[SceneDropdown.value].text;
+        //The scene must also be added to the build settings list of scenes
+        SceneManager.LoadSceneAsync(SceneName, new LoadSceneParameters(LoadSceneMode.Additive));
+    }
 
-	//The ArcGISMapView object gets instantiated in our scenes and that results in the object living in the SampleViewer scene,
-	//not the scene we loaded. To work around this we need to remove it before loading the next scene
-	private void RemoveArcGISMapView()
-	{
-		var ActiveScene = SceneManager.GetActiveScene();
-		var RootGOs = ActiveScene.GetRootGameObjects();
-		foreach (var RootGO in RootGOs)
-		{
-			var HP = RootGO.GetComponent<HPRoot>();
-			if (HP != null)
-			{
-				Destroy(RootGO);
-			}
-		}
-	}
+    //The ArcGISMapView object gets instantiated in our scenes and that results in the object living in the SampleViewer scene,
+    //not the scene we loaded. To work around this we need to remove it before loading the next scene
+    private void RemoveArcGISMapView()
+    {
+        var ActiveScene = SceneManager.GetActiveScene();
+        var RootGOs = ActiveScene.GetRootGameObjects();
+        foreach (var RootGO in RootGOs)
+        {
+            var HP = RootGO.GetComponent<HPRoot>();
+            if (HP != null)
+            {
+                Destroy(RootGO);
+            }
+        }
+    }
 
-	private void SceneChanged()
-	{
-		var DoneUnLoadingOperation = SceneManager.UnloadSceneAsync(SceneName);
-		DoneUnLoadingOperation.completed += (AsyncOperation Operation) =>
-		{
-			RemoveArcGISMapView();
-			AddScene();
-		};
-	}
+    private void SceneChanged()
+    {
+        var DoneUnLoadingOperation = SceneManager.UnloadSceneAsync(SceneName);
+        DoneUnLoadingOperation.completed += (AsyncOperation Operation) =>
+        {
+            RemoveArcGISMapView();
+            AddScene();
+        };
+    }
 
-	void PipelineChanged()
-	{
-		PipelineType = PipelineTypeDropdown.options[PipelineTypeDropdown.value].text;
-		RenderPipelineAsset pipeline = Resources.Load<RenderPipelineAsset>("SampleGraphicSettings/Sample" + PipelineType + "ipeline");
-		GraphicsSettings.renderPipelineAsset = pipeline;
+    private void PipelineChanged()
+    {
+        PipelineType = PipelineTypeDropdown.options[PipelineTypeDropdown.value].text;
+        RenderPipelineAsset pipeline = Resources.Load<RenderPipelineAsset>("SampleGraphicSettings/Sample" + PipelineType + "ipeline");
+        GraphicsSettings.renderPipelineAsset = pipeline;
 
-		if (PipelineType == "HDRP")
-		{
-			LightingObject.SetActive(true);
-		}
-		else
-		{
-			LightingObject.SetActive(false);
-		}
+        if (PipelineType == "HDRP")
+        {
+            LightingObject.SetActive(true);
+        }
+        else
+        {
+            LightingObject.SetActive(false);
+        }
 
-		SceneChanged();
-	}
+        SceneChanged();
+    }
 }
