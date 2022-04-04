a9e4fa3d8d6f3cf79e8ee5afad60fd74be722f87
Zack Allen <zack.m.allen@gmail.com>
2022-04-04 14:48:13 -0700

    Sample Viewer: Update folder structure. (#5)
    * Moved scenes
    
    * Updated switcher to have more flexible scene load
    
    Ran codemaid on this too
    
    * Copyright and further cleanup
    
    * Update EditorBuildSettings.asset
    
    * Added dummy sample for Line of sight
    
    * Origin and camera to NYC
    
    * Added placeholder screenshot and readme

--- samples_project/Assets/SampleViewer/SampleSwitcher.cs
+++ samples_project/Assets/SampleViewer/SampleSwitcher.cs
@@ -1,103 +1,114 @@
-using System.IO;
-using System.Collections;
+// Copyright 2021 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+// language governing permissions and limitations under the License.
+
+using Esri.HPFramework;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.Rendering;
-using UnityEngine.Events;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using Esri.HPFramework;
+using UnityEngine.UI;
 
 public class SampleSwitcher : MonoBehaviour
 {
-	public Dropdown PipelineTypeDropdown;
-	public Dropdown SceneDropdown;
-	public GameObject LightingObject;
-	private string PipelineType;
-	private string SceneName;
+    public Dropdown PipelineTypeDropdown;
+    public Dropdown SceneDropdown;
+    public GameObject LightingObject;
+    private string PipelineType;
+    private string SceneName;
 
-
-	void Start()
+    private void Start()
     {
-		SceneDropdown.onValueChanged.AddListener(delegate {
-			SceneChanged();
-		});
+        SceneDropdown.onValueChanged.AddListener(delegate
+        {
+            SceneChanged();
+        });
 
-		PipelineTypeDropdown.onValueChanged.AddListener(delegate {
-			PipelineChanged();
-		});
-		PipelineType = PipelineTypeDropdown.options[PipelineTypeDropdown.value].text;
+        PipelineTypeDropdown.onValueChanged.AddListener(delegate
+        {
+            PipelineChanged();
+        });
+        PipelineType = PipelineTypeDropdown.options[PipelineTypeDropdown.value].text;
 
-		PopulateSampleSceneList();
-	}
+        PopulateSampleSceneList();
+    }
 
-	private void PopulateSampleSceneList()
-	{
-		SceneDropdown.options.Clear();
-		var ApplicationPath = Application.dataPath;
-		var SamplePath = ApplicationPath + "/SampleViewer/Resources/SampleScenes/";
-		List<string> SceneList = new List<string>();
-		if (Directory.Exists(SamplePath))
-		{
-			var Scenes = Directory.EnumerateFiles(SamplePath, "*.unity");
-			foreach (string CurrentFile in Scenes)
-			{
-				string FileName = CurrentFile.Substring(SamplePath.Length, CurrentFile.Length - (".unity").Length - SamplePath.Length);
-				SceneList.Add(FileName);
-			}
-		}
-		SceneDropdown.AddOptions(SceneList);
-		AddScene();
-	}
+    private void PopulateSampleSceneList()
+    {
+        SceneDropdown.options.Clear();
 
-	private void AddScene()
-	{
-		SceneName = SceneDropdown.options[SceneDropdown.value].text;
-		//The scene must also be added to the build settings list of scenes
-		SceneManager.LoadSceneAsync(SceneName, new LoadSceneParameters(LoadSceneMode.Additive));
-	}
+        // Make a list of the formal names of the samples.
+        var ApplicationPath = Application.dataPath;
+        var SamplePath = ApplicationPath + "/SampleViewer/Samples/";
+        List<string> SceneList = new List<string>();
+        if (Directory.Exists(SamplePath))
+        {
+            var SampleScenePaths = Directory.EnumerateFiles(SamplePath, "*.unity", SearchOption.AllDirectories);
+            foreach (string CurrentFile in SampleScenePaths)
+            {
+                string SceneName = Path.GetFileNameWithoutExtension(CurrentFile);
+                SceneList.Add(SceneName);
+            }
+        }
 
-	//The ArcGISMapView object gets instantiated in our scenes and that results in the object living in the SampleViewer scene,
-	//not the scene we loaded. To work around this we need to remove it before loading the next scene
-	private void RemoveArcGISMapView()
-	{
-		var ActiveScene = SceneManager.GetActiveScene();
-		var RootGOs = ActiveScene.GetRootGameObjects();
-		foreach (var RootGO in RootGOs)
-		{
-			var HP = RootGO.GetComponent<HPRoot>();
-			if (HP != null)
-			{
-				Destroy(RootGO);
-			}
-		}
-	}
+        SceneDropdown.AddOptions(SceneList);
+        AddScene();
+    }
 
-	private void SceneChanged()
-	{
-		var DoneUnLoadingOperation = SceneManager.UnloadSceneAsync(SceneName);
-		DoneUnLoadingOperation.completed += (AsyncOperation Operation) =>
-		{
-			RemoveArcGISMapView();
-			AddScene();
-		};
-	}
+    private void AddScene()
+    {
+        SceneName = SceneDropdown.options[SceneDropdown.value].text;
+        //The scene must also be added to the build settings list of scenes
+        SceneManager.LoadSceneAsync(SceneName, new LoadSceneParameters(LoadSceneMode.Additive));
+    }
 
-	void PipelineChanged()
-	{
-		PipelineType = PipelineTypeDropdown.options[PipelineTypeDropdown.value].text;
-		RenderPipelineAsset pipeline = Resources.Load<RenderPipelineAsset>("SampleGraphicSettings/Sample" + PipelineType + "ipeline");
-		GraphicsSettings.renderPipelineAsset = pipeline;
+    //The ArcGISMapView object gets instantiated in our scenes and that results in the object living in the SampleViewer scene,
+    //not the scene we loaded. To work around this we need to remove it before loading the next scene
+    private void RemoveArcGISMapView()
+    {
+        var ActiveScene = SceneManager.GetActiveScene();
+        var RootGOs = ActiveScene.GetRootGameObjects();
+        foreach (var RootGO in RootGOs)
+        {
+            var HP = RootGO.GetComponent<HPRoot>();
+            if (HP != null)
+            {
+                Destroy(RootGO);
+            }
+        }
+    }
 
-		if (PipelineType == "HDRP")
-		{
-			LightingObject.SetActive(true);
-		}
-		else
-		{
-			LightingObject.SetActive(false);
-		}
+    private void SceneChanged()
+    {
+        var DoneUnLoadingOperation = SceneManager.UnloadSceneAsync(SceneName);
+        DoneUnLoadingOperation.completed += (AsyncOperation Operation) =>
+        {
+            RemoveArcGISMapView();
+            AddScene();
+        };
+    }
 
-		SceneChanged();
-	}
+    private void PipelineChanged()
+    {
+        PipelineType = PipelineTypeDropdown.options[PipelineTypeDropdown.value].text;
+        RenderPipelineAsset pipeline = Resources.Load<RenderPipelineAsset>("SampleGraphicSettings/Sample" + PipelineType + "ipeline");
+        GraphicsSettings.renderPipelineAsset = pipeline;
+
+        if (PipelineType == "HDRP")
+        {
+            LightingObject.SetActive(true);
+        }
+        else
+        {
+            LightingObject.SetActive(false);
+        }
+
+        SceneChanged();
+    }
 }
---
+++ samples_project/Assets/SampleViewer/Samples/LineOfSight/readme.md
@@ -0,0 +1,5 @@
+# Line of sight
+
+Placeholder readme for future use
+
+![Image of line of sight](LineOfSight.jpg)
