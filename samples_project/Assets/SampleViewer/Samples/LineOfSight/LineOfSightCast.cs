8b00353cfd89552c657535547e5b0354da4850f6
Zack Allen <zack.m.allen@gmail.com>
2022-04-07 15:21:03 -0700

    Cleaned up cylinder code

--- samples_project/Assets/SampleViewer/Samples/LineOfSight/LineOfSightCast.cs
+++ samples_project/Assets/SampleViewer/Samples/LineOfSight/LineOfSightCast.cs
@@ -7,18 +7,19 @@ public class LineOfSightCast : MonoBehaviour
     public Transform TargetTransform;
 
     public Transform Cylinder;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        if (TargetTransform == null) return;
-        var HitInfo = new RaycastHit();
+        if (TargetTransform == null || Cylinder == null) return;
+        
+        // Calculate the direction between the sphere and the target.
         var rayDirection = TargetTransform.position - transform.position;
+
+        // Create a RaycastHit for use in the raycast method.
+        var HitInfo = new RaycastHit();
+
+        // Raycast from the sphere to the target.
         if (Physics.Raycast(transform.position, rayDirection, out HitInfo))
         {
             // Check if the raycast hit the target object.
@@ -26,26 +27,21 @@ public class LineOfSightCast : MonoBehaviour
             {
                 // enemy can see the player!
                 Debug.Log("Hit!");
-                    
             }
             else
             {
                 // there is something obstructing the view
                 Debug.Log("No hit!");
             }
-            Debug.Log(HitInfo.distance);
 
-            // Get the hit position.
-            var direction = transform.position - TargetTransform.position;
-            var targetPoint = transform.position + direction.normalized * HitInfo.distance;
+            // Rotate the cylinder to the look towards the raycast hit.
+            Cylinder.LookAt(HitInfo.point);
 
-            // Rotate and scale the cylinder to show the raycast.
-            Cylinder.LookAt(targetPoint);
+            // Move the cylinder to halfway between the sphere and the hit point.
+            Cylinder.position = (transform.position + HitInfo.point) / 2;
 
-            Cylinder.position = transform.position - direction.normalized * HitInfo.distance/2;
-
-            //Cylinder.position = (HitInfo.transform.position + transform.position) / 2.0f;
-            Cylinder.localScale = new Vector3(Cylinder.localScale.x, Cylinder.localScale.y, HitInfo.distance/transform.localScale.z);
+            // Set the cylinder height to the distance of the ray cast.
+            Cylinder.localScale = new Vector3(Cylinder.localScale.x, Cylinder.localScale.y, HitInfo.distance);
         }
     }
 }
