31269b8ae508ab0afc48746e90bb40c4a072c601
Zack Allen <zack.m.allen@gmail.com>
2022-04-07 14:54:09 -0700

    Got basic cylinder working

--- samples_project/Assets/SampleViewer/Samples/LineOfSight/LineOfSightCast.cs
+++ samples_project/Assets/SampleViewer/Samples/LineOfSight/LineOfSightCast.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class LineOfSightCast : MonoBehaviour
 {
-    public Transform OtherTransform;
-    
+    public Transform TargetTransform;
 
+    public Transform Cylinder;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +16,13 @@ public class LineOfSightCast : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (OtherTransform == null) return;
+        if (TargetTransform == null) return;
         var HitInfo = new RaycastHit();
-        var rayDirection = OtherTransform.position - transform.position;
+        var rayDirection = TargetTransform.position - transform.position;
         if (Physics.Raycast(transform.position, rayDirection, out HitInfo))
         {
-            if (HitInfo.transform == OtherTransform)
+            // Check if the raycast hit the target object.
+            if (HitInfo.transform == TargetTransform)
             {
                 // enemy can see the player!
                 Debug.Log("Hit!");
@@ -32,6 +33,19 @@ public class LineOfSightCast : MonoBehaviour
                 // there is something obstructing the view
                 Debug.Log("No hit!");
             }
+            Debug.Log(HitInfo.distance);
+
+            // Get the hit position.
+            var direction = transform.position - TargetTransform.position;
+            var targetPoint = transform.position + direction.normalized * HitInfo.distance;
+
+            // Rotate and scale the cylinder to show the raycast.
+            Cylinder.LookAt(targetPoint);
+
+            Cylinder.position = transform.position - direction.normalized * HitInfo.distance/2;
+
+            //Cylinder.position = (HitInfo.transform.position + transform.position) / 2.0f;
+            Cylinder.localScale = new Vector3(Cylinder.localScale.x, Cylinder.localScale.y, HitInfo.distance/transform.localScale.z);
         }
     }
 }
