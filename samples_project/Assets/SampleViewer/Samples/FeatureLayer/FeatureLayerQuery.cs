59fce44f6f4c6faf6daf2a2551cc383cc5e30ae9
Jeffrey Scarmazzi <jeffreyscarmazzi@gmail.com>
2022-05-11 15:08:44 -0400

    Spelling & Route Request Plumbing

--- samples_project/Assets/SampleViewer/Samples/FeatureLayer/FeatureLayerQuery.cs
+++ samples_project/Assets/SampleViewer/Samples/FeatureLayer/FeatureLayerQuery.cs
@@ -72,7 +72,7 @@ public class FeatureLayerQuery : MonoBehaviour
 
         StadiumSelector.onValueChanged.AddListener(delegate
         {
-            StadiumSelcted();
+            StadiumSelected();
         });
     }
 
@@ -194,7 +194,7 @@ public class FeatureLayerQuery : MonoBehaviour
     }
 
     // When a new entry is selected in the stadium dropdown move the camera to the new position
-    private void StadiumSelcted()
+    private void StadiumSelected()
     {
         var StadiumName = StadiumSelector.options[StadiumSelector.value].text;
         foreach (GameObject Stadium in Stadiums)
@@ -211,6 +211,8 @@ public class FeatureLayerQuery : MonoBehaviour
                 NewPosition.Z = StadiumSpawnHeight;
                 CameraLocation.Position = NewPosition;
                 CameraLocation.Rotation = StadiumLocation.Rotation;
+
+                RouteManager.Instance.ActiveStadium = NewPosition;
             }
         }
     }
