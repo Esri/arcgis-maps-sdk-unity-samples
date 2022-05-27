e2c3e3cdd5e1bc4524f6485c2d092cdcf647171d
Morakot Pilouk <mpilouk@esri.com>
2022-05-27 10:08:30 -0700

    Used Text Mesh Pro instead of UI Text

--- samples_project/Assets/SampleViewer/Samples/StreamLayer/NameLabel.cs
+++ samples_project/Assets/SampleViewer/Samples/StreamLayer/NameLabel.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 public class NameLabel : MonoBehaviour
 {
 	public string nameLabel;
-	public Text text;
+	public TMP_Text text;
 	public Slider slider;
 
 	private void Start()
