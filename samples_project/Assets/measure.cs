8f10f3a232369fc308c147bbd31acd1742d89c92
Jade Huang <jad10309@esri.com>
2022-06-26 19:20:49 -0700

    update code and scene

--- samples_project/Assets/measure.cs
+++ samples_project/Assets/measure.cs
@@ -4,11 +4,11 @@
 // You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
 //
 
-using System.Net.Http;
+
 using System.Collections;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-
+using UnityEngine.UI;
 using Esri.ArcGISMapsSDK.Components;
 using Esri.ArcGISMapsSDK.Utils.GeoCoord;
 using Esri.GameEngine.Geometry;
@@ -17,29 +17,30 @@ using Esri.HPFramework;
 using UnityEngine;
 using TMPro;
 
-using Newtonsoft.Json.Linq;
 using Unity.Mathematics;
 
 public class measure : MonoBehaviour
 {
     public GameObject Route;
-    public GameObject RouteInfo;
     public string apiKey;
+    public Text txt;
     public GameObject RouteMarker;
+    public GameObject InterpolationMarker;
     private HPRoot hpRoot;
     private ArcGISMapComponent arcGISMapComponent;
     private GameObject lastStop;
     private float elevationOffset = 20.0f;
     private List<GameObject> featurePoints = new List<GameObject>();
-    private Stack<GameObject> stops=new Stack<GameObject>();
+    private Stack<GameObject> stops = new Stack<GameObject>();
     private double distance;
     private ArcGISPoint thisPoint;
     private ArcGISPoint lastPoint;
     private ArcGISLocationComponent lastStopLocation;
     private LineRenderer lineRenderer;
-    public GameObject FeaturePoint;
+    private List<LineRenderer> lines;
+    GameObject FeaturePoint;
     ArcGISSpatialReference spatialRef = new ArcGISSpatialReference(3857);
-    private HttpClient client = new HttpClient();
+    private Vector3 midPosition;
 
     double3 lastRootPosition;
 
@@ -56,7 +57,7 @@ public class measure : MonoBehaviour
 
         lastRootPosition = arcGISMapComponent.GetComponent<HPRoot>().RootUniversePosition;
         distance = 0;
-       
+
     }
 
     void Update()
@@ -76,38 +77,44 @@ public class measure : MonoBehaviour
                 routeMarker.GetComponent<ArcGISLocationComponent>().enabled = true;
                 routeMarker.GetComponent<ArcGISLocationComponent>().Position = geoPosition;
                 routeMarker.GetComponent<ArcGISLocationComponent>().Rotation = new ArcGISRotation(0, 90, 0);
-                
+
                 var thisPoint = new ArcGISPoint(geoPosition.X, geoPosition.Y, geoPosition.Z, spatialRef);
-                
-               
+
+
                 if (stops.Count > 0)
                 {
+                    featurePoints.Add(routeMarker);
+
+                    //calculating distance
                     lastStop = stops.Peek();
                     lastStopLocation = lastStop.GetComponent<ArcGISLocationComponent>();
                     lastPoint = new ArcGISPoint(lastStopLocation.Position.X, lastStopLocation.Position.Y, lastStopLocation.Position.Z, spatialRef);
                     distance = distance + ArcGISGeometryEngine.Distance(lastPoint, thisPoint);
+                    txt.text = distance.ToString();
 
-                    featurePoints.Add(routeMarker);
-                    featurePoints.Add(lastStop);
-                    Insert(lastStop, routeMarker, ref featurePoints);
+
+                    Insert(lastStop, routeMarker, featurePoints);
                     SetBreadcrumbHeight();
-                    RenderLine();
-                    
-                    
+                    RenderLine(ref featurePoints);
+                    RebaseRoute();
+                    //featurePoints.Clear();
+
+
                 }
 
                 stops.Push(routeMarker);
-                featurePoints.Clear();
+                featurePoints.Add(routeMarker);
+
             }
         }
         if (Input.GetKey(KeyCode.KeypadEnter) || Input.GetKey(KeyCode.Return))
         {
-            featurePoints.Add(stops.Peek());
-            SetBreadcrumbHeight();
-            RenderLine();
-            RebaseRoute();
+            /* 
+             SetBreadcrumbHeight();
+             RenderLine();
+             RebaseRoute();*/
         }
-            
+
     }
 
     /// <summary>
@@ -116,45 +123,37 @@ public class measure : MonoBehaviour
     /// <param name="hit"></param>
     /// <returns></returns>
 
-    private void Insert(GameObject start,GameObject end, ref List<GameObject> featurePoints)
+    private void Insert(GameObject start, GameObject end, List<GameObject> featurePoints)
     {
+        //calculating distance 
         ArcGISLocationComponent startLocation = start.GetComponent<ArcGISLocationComponent>();
         ArcGISLocationComponent endLocation = end.GetComponent<ArcGISLocationComponent>();
         ArcGISPoint startPoint = new ArcGISPoint(startLocation.Position.X, startLocation.Position.Y, startLocation.Position.Z, spatialRef);
         ArcGISPoint endPoint = new ArcGISPoint(endLocation.Position.X, endLocation.Position.Y, endLocation.Position.Z, spatialRef);
-        double d= ArcGISGeometryEngine.Distance(startPoint, endPoint);
-        if (d < 50)
+        double d = ArcGISGeometryEngine.Distance(startPoint, endPoint);
+        if (d < 200)
             return;
-        if (startLocation.Position.X > endLocation.Position.X)
-            swap(ref startLocation, ref endLocation);
-        GameObject mid = Instantiate(FeaturePoint, arcGISMapComponent.transform);
-        double midX=(math.abs(startLocation.Position.X)- math.abs(endLocation.Position.X))/2;
-        double midY = (startLocation.Position.Y - endLocation.Position.Y)/2;
-        //mid.GetComponent<ArcGISLocationComponent>().Position = GetMiddlePoint(startPoint.Y, startPoint.X, endPoint.Y, endPoint.X);
-        mid.GetComponent<ArcGISLocationComponent>().Position = new ArcGISPoint(midX,midY,spatialRef);
+
+        //calculating geolocation for next loop
+        GameObject mid = Instantiate(InterpolationMarker, arcGISMapComponent.transform);
+        double midLocationComponentX = startLocation.Position.X + (endLocation.Position.X - startLocation.Position.X) / 2;
+        double midLocaitonComponentY = startLocation.Position.Y + (endLocation.Position.Y - startLocation.Position.Y) / 2;
+        mid.GetComponent<ArcGISLocationComponent>().enabled = true;
+        mid.GetComponent<ArcGISLocationComponent>().Position = new ArcGISPoint(midLocationComponentX, midLocaitonComponentY, 0, spatialRef);
         mid.GetComponent<ArcGISLocationComponent>().Rotation = new ArcGISRotation(0, 90, 0);
+
+        //calculating unity location to draw points 
+        float midX = start.transform.position.x + (end.transform.position.x - start.transform.position.x) / 2;
+        float midY = start.transform.position.y + (end.transform.position.y - start.transform.position.y) / 2;
+        float midZ = start.transform.position.z + (end.transform.position.z - start.transform.position.z) / 2;
+        midPosition = new Vector3(midX, midY, midZ);
+        mid.transform.position = midPosition;
+
+
         featurePoints.Add(mid);
-        Insert(start, mid, ref featurePoints);
-        Insert(mid, end, ref featurePoints);
+        Insert(start, mid, featurePoints);
+        Insert(mid, end, featurePoints);
 
-    }
-
-    private ArcGISPoint GetMiddlePoint(double lat1, double lon1, double lat2, double lon2)
-    {
-        double dLon = (math.PI /180)*(lon2 - lon1);
-        lat1 = (math.PI / 180) * lat1;
-        lat2 = (math.PI / 180) * lat2;
-        lon1 = (math.PI / 180) * lon1;
-
-        double Bx = math.cos(lat2) * math.cos(dLon);
-        double By = math.cos(lat2) * math.sin(dLon);
-        double lat3 = math.atan2(math.sin(lat1) + math.sin(lat2), math.sqrt((math.cos(lat1) + Bx) * (math.cos(lat1) + Bx) + By * By));
-        double lon3 = lon1 + math.atan2(By, math.cos(lat1) + Bx);
-
-        double lat = (180 / math.PI) * lat3;
-        double lon= (180 / math.PI) * lon3;
-        ArcGISPoint midPoint = new ArcGISPoint(lon,lat,spatialRef);
-        return midPoint;
     }
 
     private ArcGISPoint HitToGeoPosition(RaycastHit hit, float yOffset = 0)
@@ -167,8 +166,6 @@ public class measure : MonoBehaviour
         return GeoUtils.ProjectToSpatialReference(offsetPosition, spatialRef);
     }
 
-    // Does a raycast to get the elevation for each point.  For routes covering long distances the raycast will only hit elevation that is actively loaded. If you are doing 
-    // something like this the raycast needs to happen dynamically when the data is loaded. This can be accomplished by only raycasting for breadcrums within a distance of the camera.
     private void SetBreadcrumbHeight()
     {
         for (int i = 0; i < featurePoints.Count; i++)
@@ -202,9 +199,10 @@ public class measure : MonoBehaviour
         if (lineRenderer)
             lineRenderer.positionCount = 0;*/
     }
-    
-    private void RenderLine()
+
+    private void RenderLine(ref List<GameObject> featurePoints)
     {
+        //lineRenderer.positionCount = 0;
         lineRenderer.widthMultiplier = 5;
 
         var allPoints = new List<Vector3>();
@@ -218,7 +216,7 @@ public class measure : MonoBehaviour
             }
             allPoints.Add(stop.transform.position);
         }
-        
+
         lineRenderer.positionCount = allPoints.Count;
         lineRenderer.SetPositions(allPoints.ToArray());
     }
