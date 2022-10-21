afaa2aba4fbd977c8826d8ff9c0863c535a8c773
Ben Bair <bbair@esri.com>
2022-10-20 21:48:34 -0700

    Draft UI to control observer params (UI only)

---
+++ samples_project/Assets/SampleViewer/Samples/Viewshed/Scripts/SliderText.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderText : MonoBehaviour
+{
+
+    public string prefix = "";
+
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
