46275a14636bb3617e8ce19834ef800cbbee351f
Sam Cartledge <scartledge@esri.com>
2022-10-25 12:27:32 -0700

    add dialog for mac

--- samples_project/Assets/Editor/BuildSamplesError.cs
+++ samples_project/Assets/Editor/BuildSamplesError.cs
@@ -17,6 +17,10 @@ public class BuildSamplesError
         EditorUtility.DisplayDialog("Pipeline Error:", "\nBuilding with both render pipelines installed is not available. Please remove the HDRP package if building for a mobile device, or remove the URP package if building for Windows or MacOS.", "OK");
         
         throw new BuildFailedException("Cannot build with both render pipeline packages installed. Please remove one.");
+#elif USE_OPENXR_PACKAGE && UNITY_STANDALONE_OSX
+        EditorUtility.DisplayDialog("OpenXR Error:", "\nCannot build for MacOS standalone with OpenXR installed. Please remove the OpenXR package with the Package Manager, and uncheck \"VR-Sample" from the Build Settings scene list", "OK");
+        
+        throw new BuildFailedException("Cannot build with OpenXR package installed. Please remove before building for MacOS standalone.");
 #else
         BuildPipeline.BuildPlayer(options);
 #endif
