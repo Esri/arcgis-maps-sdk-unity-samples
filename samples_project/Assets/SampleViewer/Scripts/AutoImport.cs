82d2ec96b29fe6f180b18e0a2fded4f62793dec2
Sam Cartledge <scartledge@esri.com>
2022-11-16 11:34:53 -0800

    Update AutoImport.cs
    Reimports all sample files instead of only specific files

--- samples_project/Assets/SampleViewer/Scripts/AutoImport.cs
+++ samples_project/Assets/SampleViewer/Scripts/AutoImport.cs
@@ -45,12 +45,7 @@ public static class CallReImport
     static void reImport()
     {
         List<string> importPaths = new List<string>();
-        importPaths.Add("Assets/SampleViewer/Samples/FeatureLayer/StadiumMaterial.mat");
-        importPaths.Add("Assets/SampleViewer/Samples/FeatureLayer/StadiumShader.shadergraph");
-        importPaths.Add("Assets/SampleViewer/Samples/LineOfSight/Materials");
-        importPaths.Add("Assets/SampleViewer/Samples/Routing/Breadcrumb.mat");
-        importPaths.Add("Assets/SampleViewer/Samples/Routing/MarkerBody.mat");
-        importPaths.Add("Assets/SampleViewer/Samples/Routing/MarkerHead.mat");
+        importPaths.Add("Assets/SampleViewer/Samples");
         importPaths.Add("Packages/com.esri.arcgis-maps-sdk/SDK/Resources/Shaders/Materials/URP");
         importPaths.Add("Assets/Samples/ArcGIS Maps SDK for Unity/1.0.0/All Samples/Resources/Materials/URP");
         importPaths.Add("Packages/com.esri.arcgis-maps-sdk/SDK/Resources/Shaders/Materials/HDRP");
