2d21abec69bc9511a7c93d6246f81427325dbf2c
mas12517 <89485705+mmgaw113@users.noreply.github.com>
2023-07-26 15:07:52 -0700

    Setup VR UI

--- samples_project/Assets/SampleViewer/Samples/VRSample/Scripts/ContinuousMovement.cs
+++ samples_project/Assets/SampleViewer/Samples/VRSample/Scripts/ContinuousMovement.cs
@@ -1,52 +1,97 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 using Unity.XR.CoreUtils;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
+using CommonUsages = UnityEngine.XR.CommonUsages;
 
 [RequireComponent(typeof(CharacterController))]
 public class ContinuousMovement : MonoBehaviour
 {
-    public XRNode leftInputSource;
-    public XRNode rightInputSource;
+    public XRNode LeftInputSource;
+    public XRNode RightInputSource;
+    public InputAction MenuButtonReference;
+    public bool UseSmoothTurning = true;
+    private ContinuousTurnProviderBase ContinuousTurnProviderBase;
     private Vector2 leftInputAxis;
     private Vector2 rightInputAxis;
     private CharacterController controller;
     [SerializeField] private float speed;
     [SerializeField] private float upSpeed;
     private XROrigin rig;
-    public LayerMask groundLayer;
     private float fallSpeed;
+    private SnapTurnProviderBase SnapTurnProviderBase;
     private float heightOffset = 0.2f;
+
+    private void Awake()
+    {
+        ContinuousTurnProviderBase = GameObject.Find("Locomotion System").GetComponent<ContinuousTurnProviderBase>();
+        SnapTurnProviderBase = GameObject.Find("Locomotion System").GetComponent<SnapTurnProviderBase>();
+    }
+
+    private void OnEnable()
+    {
+        MenuButtonReference.Enable();
+    }
+
+    private void OnDisable()
+    {
+        MenuButtonReference.Disable();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
         rig = GetComponent<XROrigin>();
+        ToggleSmoothTurn();
     }
+
     private void FixedUpdate()
     {
         FollowHeadset();
-        Quaternion headYaw = Quaternion.Euler(0, rig.Camera.transform.eulerAngles.y, 0);
-        Vector3 direction = headYaw * new Vector3(leftInputAxis.x, 0, leftInputAxis.y);
-        Vector3 up = new Vector3(0, rightInputAxis.y, 0);
-        controller.Move(direction * speed * Time.fixedDeltaTime);
-        controller.Move(up * upSpeed * Time.fixedDeltaTime);
+        var headYaw = Quaternion.Euler(0, rig.Camera.transform.eulerAngles.y, 0);
+        var direction = headYaw * new Vector3(leftInputAxis.x, 0, leftInputAxis.y);
+        var up = new Vector3(0, rightInputAxis.y, 0);
+        controller.Move( speed * Time.fixedDeltaTime * direction);
+        controller.Move(upSpeed * Time.fixedDeltaTime * up);
 
     }
+
     // Update is called once per frame
     void Update()
     {
-        InputDevice leftDevice = InputDevices.GetDeviceAtXRNode(leftInputSource);
+        var leftDevice = InputDevices.GetDeviceAtXRNode(LeftInputSource);
         leftDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out leftInputAxis);
-        InputDevice rightDevice = InputDevices.GetDeviceAtXRNode(rightInputSource);
+        var rightDevice = InputDevices.GetDeviceAtXRNode(RightInputSource);
         rightDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out rightInputAxis);
+        if (MenuButtonReference.triggered)
+        {
+            Debug.Log("button pressed");
+        }
     }
+
     void FollowHeadset()
     {
         controller.height = rig.CameraInOriginSpaceHeight + heightOffset;
-        Vector3 capsuleCenter = transform.InverseTransformPoint(rig.Camera.transform.position);
+        var capsuleCenter = transform.InverseTransformPoint(rig.Camera.transform.position);
         controller.center = new Vector3(capsuleCenter.x, controller.height / 2 + controller.skinWidth, capsuleCenter.z);
     }
+
+    private void ToggleSmoothTurn()
+    {
+        if (UseSmoothTurning)
+        {
+            ContinuousTurnProviderBase.enabled = true;
+            SnapTurnProviderBase.enabled = false;
+        }
+        else
+        {
+            ContinuousTurnProviderBase.enabled = false;
+            SnapTurnProviderBase.enabled = true;
+        }
+    }
+
 }
