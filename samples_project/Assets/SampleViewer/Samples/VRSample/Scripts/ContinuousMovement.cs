da667915da01d7ffc95d3196d179b6e18cdd0cd9
mas12517 <89485705+mmgaw113@users.noreply.github.com>
2023-07-26 17:42:29 -0700

    UI Setup and Fix Map Issue

--- samples_project/Assets/SampleViewer/Samples/VRSample/Scripts/ContinuousMovement.cs
+++ samples_project/Assets/SampleViewer/Samples/VRSample/Scripts/ContinuousMovement.cs
@@ -1,25 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 using Unity.XR.CoreUtils;
+using UnityEngine.InputSystem;
+using CommonUsages = UnityEngine.XR.CommonUsages;
 
 [RequireComponent(typeof(CharacterController))]
 public class ContinuousMovement : MonoBehaviour
 {
     public XRNode leftInputSource;
     public XRNode rightInputSource;
+    public InputAction LeftMenuAction;
+    public InputAction RightMenuAction;
+    public GameObject UICanvas;
+    public bool UseSmoothTurn;
     private Vector2 leftInputAxis;
     private Vector2 rightInputAxis;
     private CharacterController controller;
     [SerializeField] private float speed;
     [SerializeField] private float upSpeed;
     private XROrigin rig;
-    public LayerMask groundLayer;
     private float fallSpeed;
     private float heightOffset = 0.2f;
-    // Start is called before the first frame update
+    private bool toggledOn = true;
+    private ContinuousTurnProviderBase smoothTurn;
+    private SnapTurnProviderBase snapTurn;
+
+    private void Awake()
+    {
+        smoothTurn = GameObject.Find("Locomotion System").GetComponent<ContinuousTurnProviderBase>();
+        snapTurn = GameObject.Find("Locomotion System").GetComponent<SnapTurnProviderBase>();
+        ToggleSmoothTurn();
+    }
+
+    private void OnEnable()
+    {
+        LeftMenuAction.Enable();
+        RightMenuAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        LeftMenuAction.Disable();
+        RightMenuAction.Disable();
+    }
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -28,25 +52,63 @@ public class ContinuousMovement : MonoBehaviour
     private void FixedUpdate()
     {
         FollowHeadset();
-        Quaternion headYaw = Quaternion.Euler(0, rig.Camera.transform.eulerAngles.y, 0);
-        Vector3 direction = headYaw * new Vector3(leftInputAxis.x, 0, leftInputAxis.y);
-        Vector3 up = new Vector3(0, rightInputAxis.y, 0);
-        controller.Move(direction * speed * Time.fixedDeltaTime);
-        controller.Move(up * upSpeed * Time.fixedDeltaTime);
+        var headYaw = Quaternion.Euler(0, rig.Camera.transform.eulerAngles.y, 0);
+        var direction = headYaw * new Vector3(leftInputAxis.x, 0, leftInputAxis.y);
+        var up = new Vector3(0, rightInputAxis.y, 0);
+        controller.Move(speed * Time.fixedDeltaTime * direction);
+        controller.Move(upSpeed * Time.fixedDeltaTime * up);
+        if (LeftMenuAction.triggered)
+        {
+            ToggleCanvas();
+        }
+        if (RightMenuAction.triggered)
+        {
+            ToggleCanvas();
+        }
 
     }
     // Update is called once per frame
     void Update()
     {
-        InputDevice leftDevice = InputDevices.GetDeviceAtXRNode(leftInputSource);
+        var leftDevice = InputDevices.GetDeviceAtXRNode(leftInputSource);
         leftDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out leftInputAxis);
-        InputDevice rightDevice = InputDevices.GetDeviceAtXRNode(rightInputSource);
+        var rightDevice = InputDevices.GetDeviceAtXRNode(rightInputSource);
         rightDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out rightInputAxis);
     }
     void FollowHeadset()
     {
         controller.height = rig.CameraInOriginSpaceHeight + heightOffset;
-        Vector3 capsuleCenter = transform.InverseTransformPoint(rig.Camera.transform.position);
+        var capsuleCenter = transform.InverseTransformPoint(rig.Camera.transform.position);
         controller.center = new Vector3(capsuleCenter.x, controller.height / 2 + controller.skinWidth, capsuleCenter.z);
+    }
+
+    public void ToggleSmoothTurn()
+    {
+        if (!UseSmoothTurn)
+        {
+            smoothTurn.enabled = true;
+            snapTurn.enabled = false;
+            UseSmoothTurn = true;
+        }
+        else
+        {
+            smoothTurn.enabled = false;
+            snapTurn.enabled = true;
+            UseSmoothTurn = false;
+        }
+    }
+
+    public void ToggleCanvas()
+    {
+        if (!toggledOn)
+        {
+            UICanvas.SetActive(true);
+            toggledOn = true;
+        }
+        else
+        {
+            UICanvas.SetActive(false);
+            toggledOn = false;
+        }
     }
 }
