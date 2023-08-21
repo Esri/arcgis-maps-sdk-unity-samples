9499facc08d0daf380158ba948b2d883aec60920
Jade Huang <jadehuang@esri.com>
2023-08-21 13:38:45 -0700

    improve pin positioning in reverse geocoding  (#85)
    * Initial commit
    
    * adding image
    
    * Update AddressCard.prefab
    
    * Geocoding quick update
    
    * Update Geocoding.unity
    
    * Geocoding UI refinement
    
    * Update Geocoding.unity
    
    * Update Geocoding.unity
    
    * Darken the menu bg and add the info icon to bring notification menu back
    
    * Update SampleViewer.unity
    
    * correct typo and change icon in Sample Viewer
    
    * initial update for the dropdown menu in feature layer sample
    
    * refine feature layer sample UI
    
    * Update FeatureLayer.unity
    
    * dropdown prefab update
    
    * Update FeatureLayer.unity
    
    * fix UI interaction and address initial feedback
    
    * Update FeatureLayerQuery.cs
    
    * update
    
    * Misc. fix
    
    * use pin and improve positioning
    
    * minor fix
    
    ---------
    
    Co-authored-by: Andy Ren <aren@esri.com>
    Co-authored-by: Jade Huang <jad10309@esri.com>

--- samples_project/Assets/SampleViewer/Samples/Geocoding/Geocoder.cs
+++ samples_project/Assets/SampleViewer/Samples/Geocoding/Geocoder.cs
@@ -47,6 +47,7 @@ public class Geocoder : MonoBehaviour
     private bool ShouldPlaceMarker = false;
     private bool WaitingForResponse = false;
     private float Timer = 0;
+    private float DistanceFromCamera;
     private readonly float MapLoadWaitTime = 1;
     private readonly string AddressQueryURL = "https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates";
     private readonly string LocationQueryURL = "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode";
@@ -95,9 +96,12 @@ public class Geocoder : MonoBehaviour
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
                 Vector3 direction = (hit.point - MainCamera.transform.position);
-                float distanceFromCamera = Vector3.Distance(MainCamera.transform.position, hit.point);
-                float scale = distanceFromCamera * LocationMarkerScale / 5000; // Scale the marker based on its distance from camera 
-                SetupQueryLocationGameObject(LocationMarkerTemplate, hit.point, MainCamera.transform.rotation, new Vector3(scale, scale, scale));
+                DistanceFromCamera = Vector3.Distance(MainCamera.transform.position, hit.point);
+                float scale = DistanceFromCamera * LocationMarkerScale / 400000; // Scale the marker based on its distance from camera 
+                Quaternion markerRotationPerpendicular = Quaternion.FromToRotation(Vector3.up, hit.normal);
+                Quaternion markerRotationFacingCamera = MainCamera.transform.rotation;
+                Quaternion markerRotation = markerRotationPerpendicular * markerRotationFacingCamera;
+                SetupQueryLocationGameObject(LocationMarkerTemplate, hit.point, markerRotation, new Vector3(scale, scale, scale));
                 ReverseGeocode(HitToGeoPosition(hit));
             }
         }
@@ -363,8 +367,8 @@ public class Geocoder : MonoBehaviour
         }
         else
         {
-            float localScale = 3.5f / LocationMarkerScale;
-            card.transform.localPosition = new Vector3(0, 300f / LocationMarkerScale, -300f / LocationMarkerScale);
+            float localScale = LocationMarkerScale / 40;
+            card.transform.localPosition = new Vector3(0, 300f / LocationMarkerScale + 400, -300f / LocationMarkerScale);
             card.transform.localScale = new Vector3(localScale, localScale, localScale);
         }
 
