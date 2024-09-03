1718a656364d94a3986d3880c8e9acb45c104bd2
mmgaw13 <moose.gaw@gmail.com>
2024-09-03 10:24:25 -0700

    Address feedback

--- sample_project/Assets/SampleViewer/Scripts/SampleSwitcher.cs
+++ sample_project/Assets/SampleViewer/Scripts/SampleSwitcher.cs
@@ -26,9 +26,9 @@ public class SampleSwitcher : MonoBehaviour
     private string currentSceneName;
     private string nextSceneName;
     private string projectAPIKey;
-	private string pipelineModeText;
-	private string PipelineType;
-	private int sceneLoadedCount;
+    private string pipelineModeText;
+    private string PipelineType;
+    private int sceneLoadedCount;
 
     public GameObject MenuVideo;
     public Button ExitButton;
@@ -37,274 +37,266 @@ public class SampleSwitcher : MonoBehaviour
     public Button[] pipelineButtons;
 
     private void Start()
-	{
-		animator = notificationMenu.GetComponent<Animator>();
-		cam.enabled = true;
+    {
+        animator = notificationMenu.GetComponent<Animator>();
+        cam.enabled = true;
 
-		Invoke("SlideNotification", 2.0f);
+        Invoke("SlideNotification", 2.0f);
 
-		if (apiKey != null && apiKey.Length == 100)
-		{
-			CheckAPIKey(apiKey);
-		}
-		else 
-		{
-			projectAPIKey = ArcGISProjectSettingsAsset.Instance.APIKey;
-			CheckAPIKey(projectAPIKey);
-		}
+        if (apiKey != null && apiKey.Length == 100)
+        {
+            CheckAPIKey(apiKey);
+        }
+        else
+        {
+            projectAPIKey = ArcGISProjectSettingsAsset.Instance.APIKey;
+            CheckAPIKey(projectAPIKey);
+        }
 
-		ExitButton.onClick.AddListener(delegate
-		{
-			DoExitGame();
-		});
+        ExitButton.onClick.AddListener(delegate
+        {
+            DoExitGame();
+        });
 
 #if (UNITY_ANDROID || UNITY_IOS || UNITY_WSA)
         SetPipeline("URP");
         SetPipelineColor(1, pipelineButtons[1].colors.selectedColor, false);
 #else
-		SetPipeline("HDRP");
-		SetPipelineColor(0, pipelineButtons[0].colors.selectedColor, false);
+        SetPipeline("HDRP");
+        SetPipelineColor(0, pipelineButtons[0].colors.selectedColor, false);
 #endif
-	}
+    }
 
-	private void Update()
-	{
+    private void Update()
+    {
         // API Script handles api key differently than the mapcomponent
         var api = FindObjectOfType<APIMapCreator>();
-		if (api != null)
-		{
-			if (api.APIKey == "")
-			{
-				api.APIKey = apiKey;
-			}
-			return;
-		}
+        if (api != null)
+        {
+            if (api.APIKey == "")
+            {
+                api.APIKey = apiKey;
+            }
+            return;
+        }
 
-		var mapComponent = FindObjectOfType<ArcGISMapComponent>();
-		if (mapComponent != null && mapComponent.APIKey == "")
-		{
-			mapComponent.APIKey = apiKey;
-		}
-		sceneLoadedCount = SceneManager.sceneCount;
-	}
+        var mapComponent = FindObjectOfType<ArcGISMapComponent>();
+        if (mapComponent != null && mapComponent.APIKey == "")
+        {
+            mapComponent.APIKey = apiKey;
+        }
+        sceneLoadedCount = SceneManager.sceneCount;
+    }
 
-	private void AddScene()
-	{
-		currentSceneName = nextSceneName;
+    private void AddScene()
+    {
+        currentSceneName = nextSceneName;
 
-		//The scene must also be added to the build settings list of scenes
-		SceneManager.LoadSceneAsync(currentSceneName, new LoadSceneParameters(LoadSceneMode.Additive));
-	}
+        //The scene must also be added to the build settings list of scenes
+        SceneManager.LoadSceneAsync(currentSceneName, new LoadSceneParameters(LoadSceneMode.Additive));
+    }
 
-	//The ArcGISMapView object gets instantiated in our scenes and that results in the object living in the SampleViewer scene,
-	//not the scene we loaded. To work around this we need to remove it before loading the next scene
-	private void RemoveArcGISMapView()
-	{
-		var ActiveScene = SceneManager.GetActiveScene();
-		var RootGOs = ActiveScene.GetRootGameObjects();
-		foreach (var RootGO in RootGOs)
-		{
-			var HP = RootGO.GetComponent<HPRoot>();
-			if (HP != null)
-			{
-				Destroy(RootGO);
-			}
-		}
-	}
+    //The ArcGISMapView object gets instantiated in our scenes and that results in the object living in the SampleViewer scene,
+    //not the scene we loaded. To work around this we need to remove it before loading the next scene
+    private void RemoveArcGISMapView()
+    {
+        var ActiveScene = SceneManager.GetActiveScene();
+        var RootGOs = ActiveScene.GetRootGameObjects();
+        foreach (var RootGO in RootGOs)
+        {
+            var HP = RootGO.GetComponent<HPRoot>();
+            if (HP != null)
+            {
+                Destroy(RootGO);
+            }
+        }
+    }
 
-	private void StopVideo()
-	{
-		MenuVideo.GetComponent<UnityEngine.Video.VideoPlayer>().Stop();
-		MenuVideo.gameObject.SetActive(false);
-	}
+    private void StopVideo()
+    {
+        MenuVideo.GetComponent<UnityEngine.Video.VideoPlayer>().Stop();
+        MenuVideo.gameObject.SetActive(false);
+    }
 
-	// pipelineType must be HDRP or URP
-	private void SetPipeline(string pipelineType)
-	{
-		PipelineType = pipelineType;
-		RenderPipelineAsset pipeline = Resources.Load<RenderPipelineAsset>("SampleGraphicSettings/Sample" + PipelineType + "ipeline");
-		GraphicsSettings.renderPipelineAsset = pipeline;
-	}
+    // pipelineType must be HDRP or URP
+    private void SetPipeline(string pipelineType)
+    {
+        PipelineType = pipelineType;
+        RenderPipelineAsset pipeline = Resources.Load<RenderPipelineAsset>("SampleGraphicSettings/Sample" + PipelineType + "ipeline");
+        GraphicsSettings.renderPipelineAsset = pipeline;
+    }
 
-	private IEnumerator PipelineChanged()
-	{
-		var Sky = FindObjectOfType<ArcGISSkyRepositionComponent>();
-		if (Sky != null)
-		{
-			DestroyImmediate(Sky.gameObject);
-		}
+    private IEnumerator PipelineChanged()
+    {
+        var Sky = FindObjectOfType<ArcGISSkyRepositionComponent>();
+        if (Sky != null)
+        {
+            DestroyImmediate(Sky.gameObject);
+        }
 
-		yield return null;
+        yield return null;
 
-		// Set the Pipeline based on what Pipeline button is clicked
-		SetPipeline(pipelineModeText);
+        // Set the Pipeline based on what Pipeline button is clicked
+        SetPipeline(pipelineModeText);
 
-		SceneButtonOnClick();
-	}
+        SceneButtonOnClick();
+    }
 
-	// Delay pop-up notification
-	private void SlideNotification()
-	{
-		//Play notification menu animation.
-		animator.Play("NotificationAnim");
-	}
+    // Delay pop-up notification
+    private void SlideNotification()
+    {
+        //Play notification menu animation.
+        animator.Play("NotificationAnim");
+    }
 
-	private void EnableSceneButtons()
-	{
-		foreach (Button btn in sceneButtons)
-		{
-			btn.interactable = true;
-		}
-	}
+    private void EnableSceneButtons()
+    {
+        foreach (Button btn in sceneButtons)
+        {
+            btn.interactable = true;
+        }
+    }
 
-	private void DisableSceneButtons()
-	{
-		foreach (Button btn in sceneButtons)
-		{
-			btn.interactable = false;
-		}
-	}
+    private void DisableSceneButtons()
+    {
+        foreach (Button btn in sceneButtons)
+        {
+            btn.interactable = false;
+        }
+    }
 
-	//Exits the Sample Viewer App
-	private void DoExitGame()
-	{
+    //Exits the Sample Viewer App
+    private void DoExitGame()
+    {
 #if UNITY_EDITOR
-		UnityEditor.EditorApplication.isPlaying = false;
+        UnityEditor.EditorApplication.isPlaying = false;
 #else
         Application.Quit();
 #endif
-	}
-
-	public void CheckAPIKey(string value)
-	{
-		apiKey = value;
-
-		if (apiKey == "")
-		{
-			apiKeyInputField.text = "Enter your API key here...";
-		}
-		else
-		{
-            apiKeyInputField.text = apiKey;
-        }
-
-		if(warning != null)
-		{
-			if (value.Length == 100)
-			{
-				warning.gameObject.SetActive(false);
-				EnableSceneButtons();
-			}
-			else
-			{
-				warning.gameObject.SetActive(true);
-				DisableSceneButtons();
-			}
-		}
     }
 
-	public void SetPipelineText(string text)
-	{
-		pipelineModeText = text;
-	}
+    public void CheckAPIKey(string value)
+    {
+        apiKey = value;
+        apiKeyInputField.text = string.IsNullOrEmpty(apiKey) ? "Enter your API key here..." : apiKey;
 
-	public void SetNextSceneName(string text)
-	{
-		nextSceneName = text;
-	}
+        if (warning != null)
+        {
+            if (value.Length == 100)
+            {
+                warning.gameObject.SetActive(false);
+                EnableSceneButtons();
+            }
+            else
+            {
+                warning.gameObject.SetActive(true);
+                DisableSceneButtons();
+            }
+        }
+    }
 
-	// Switch scenes with button click
-	public void SceneButtonOnClick()
-	{
-		StopVideo();
+    public void SetPipelineText(string text)
+    {
+        pipelineModeText = text;
+    }
 
-		cam.enabled = false;
+    public void SetNextSceneName(string text)
+    {
+        nextSceneName = text;
+    }
 
-		animator.Play("NotificationAnim_Close");
+    // Switch scenes with button click
+    public void SceneButtonOnClick()
+    {
+        StopVideo();
 
-		// If no async scene is running, then just load an async scene
-		if (sceneLoadedCount == 1)
-		{
-			AddScene();
-		}
-		else if (sceneLoadedCount == 2)
-		{
-			// Change scene
-			var DoneUnloadingOperation = SceneManager.UnloadSceneAsync(currentSceneName);
-			DoneUnloadingOperation.completed += (AsyncOperation Operation) =>
-			{
-				RemoveArcGISMapView();
+        cam.enabled = false;
 
-				AddScene();
-			};
-		}
-	}
+        animator.Play("NotificationAnim_Close");
 
-	public void PipelineButtonOnClick()
-	{
-		StartCoroutine(PipelineChanged());
-	}
+        // If no async scene is running, then just load an async scene
+        if (sceneLoadedCount == 1)
+        {
+            AddScene();
+        }
+        else if (sceneLoadedCount == 2)
+        {
+            // Change scene
+            var DoneUnloadingOperation = SceneManager.UnloadSceneAsync(currentSceneName);
+            DoneUnloadingOperation.completed += (AsyncOperation Operation) =>
+            {
+                RemoveArcGISMapView();
 
-	// Keep scene buttons pressed after selection
-	public void OnSceneButtonClicked(Button clickedButton)
-	{
-		int btnIndex = System.Array.IndexOf(sceneButtons, clickedButton);
+                AddScene();
+            };
+        }
+    }
 
-		if (btnIndex == -1)
-		{
-			return;
-		}
+    public void PipelineButtonOnClick()
+    {
+        StartCoroutine(PipelineChanged());
+    }
 
-		EnableSceneButtons();
+    // Keep scene buttons pressed after selection
+    public void OnSceneButtonClicked(Button clickedButton)
+    {
+        int btnIndex = System.Array.IndexOf(sceneButtons, clickedButton);
 
-		clickedButton.interactable = false;
-	}
+        if (btnIndex == -1)
+        {
+            return;
+        }
 
-	// Keep pipeline buttons pressed after selection
-	public void OnPipelineButtonClicked(Button clickedButton)
-	{
-		int btnIndex = System.Array.IndexOf(pipelineButtons, clickedButton);
+        EnableSceneButtons();
 
-		if (btnIndex == -1)
-		{
-			return;
-		}
+        clickedButton.interactable = false;
+    }
 
-		foreach (Button btn in pipelineButtons)
-		{
-			btn.interactable = true;
-		}
+    // Keep pipeline buttons pressed after selection
+    public void OnPipelineButtonClicked(Button clickedButton)
+    {
+        int btnIndex = System.Array.IndexOf(pipelineButtons, clickedButton);
 
-		clickedButton.interactable = false;
-	}
+        if (btnIndex == -1)
+        {
+            return;
+        }
 
-	// Set HDRP/URP button color
-	public void SetPipelineColor(int index, Color color, bool active)
-	{
-		var colors = pipelineButtons[index].colors;
-		colors.normalColor = color;
-		pipelineButtons[index].colors = colors;
-		pipelineButtons[index].interactable = active;
-	}
+        foreach (Button btn in pipelineButtons)
+        {
+            btn.interactable = true;
+        }
 
-	// Unload HDRP/URP button color
-	public void UnloadPipelineColor(int index, Color color, bool active)
-	{
-		var colors = pipelineButtons[index].colors;
-		colors.normalColor = new Color(0.498f, 0.459f, 0.588f, 1.0f);
-		pipelineButtons[index].colors = colors;
-		pipelineButtons[index].interactable = active;
-	}
+        clickedButton.interactable = false;
+    }
 
-	// Unload HDRP button color
-	public void UnloadHDRPColor()
-	{
-		UnloadPipelineColor(0, pipelineButtons[0].colors.normalColor, true);
-	}
+    // Set HDRP/URP button color
+    public void SetPipelineColor(int index, Color color, bool active)
+    {
+        var colors = pipelineButtons[index].colors;
+        colors.normalColor = color;
+        pipelineButtons[index].colors = colors;
+        pipelineButtons[index].interactable = active;
+    }
 
-	// Unload URP button color
-	public void UnloadURPColor()
-	{
-		UnloadPipelineColor(1, pipelineButtons[1].colors.normalColor, true);
-	}
+    // Unload HDRP/URP button color
+    public void UnloadPipelineColor(int index, Color color, bool active)
+    {
+        var colors = pipelineButtons[index].colors;
+        colors.normalColor = new Color(0.498f, 0.459f, 0.588f, 1.0f);
+        pipelineButtons[index].colors = colors;
+        pipelineButtons[index].interactable = active;
+    }
+
+    // Unload HDRP button color
+    public void UnloadHDRPColor()
+    {
+        UnloadPipelineColor(0, pipelineButtons[0].colors.normalColor, true);
+    }
+
+    // Unload URP button color
+    public void UnloadURPColor()
+    {
+        UnloadPipelineColor(1, pipelineButtons[1].colors.normalColor, true);
+    }
 }
