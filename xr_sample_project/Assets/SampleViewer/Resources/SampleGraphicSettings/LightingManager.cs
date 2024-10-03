a5a7e03c42957fb4f441b4b8d1c2287b66ada890
mmgaw13 <moose.gaw@gmail.com>
2024-10-03 10:39:29 -0700

    re add files

---
+++ xr_sample_project/Assets/SampleViewer/Resources/SampleGraphicSettings/LightingManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using Esri.ArcGISMapsSDK.Components;
+
+public class LightingManager : MonoBehaviour
+{
+    public GameObject HDRPLighting;
+    public GameObject URPLighting;
+    public bool IsCloudXRDemo;
+
+    private bool ActivePipelineIsHDRP()
+    {
+        return GraphicsSettings.renderPipelineAsset.name.Contains("HDRP");
+    }
+
+    void Start()
+    {
+        if (IsCloudXRDemo || ActivePipelineIsHDRP())
+        {
+#if USE_HDRP_PACKAGE
+            var HDRPLightingObject = Instantiate(HDRPLighting, transform);
+            var Sky = HDRPLightingObject.GetComponentInChildren<ArcGISSkyRepositionComponent>();
+            Sky.CameraComponent = FindObjectOfType<ArcGISCameraComponent>();
+            Sky.arcGISMapComponent = FindObjectOfType<ArcGISMapComponent>();
+            HDRPLightingObject.SetActive(true);
+#endif
+        }
+        else
+        {
+            var URPLightingObject = Instantiate(URPLighting, transform);
+            URPLightingObject.SetActive(true);
+        }
+    }
+}
