a86ae9685ed68d2a2bd552c3b9957c4896db0937
Zack Allen <zallen@esri.com>
2024-11-08 14:45:20 -0800

    Add attribution component to tabletop

---
+++ xr_sample_project/Assets/SampleViewer/Samples/XRTableTop/Scripts/ArcGISAttributionTextUpdater.cs
@@ -0,0 +1,80 @@
+// COPYRIGHT 1995-2024 ESRI
+// TRADE SECRETS: ESRI PROPRIETARY AND CONFIDENTIAL
+// Unpublished material - all rights reserved under the
+// Copyright Laws of the United States and applicable international
+// laws, treaties, and conventions.
+//
+// For additional information, contact:
+// Attn: Contracts and Legal Department
+// Environmental Systems Research Institute, Inc.
+// 380 New York Street
+// Redlands, California 92373
+// USA
+//
+// email: legal@esri.com
+using Esri.ArcGISMapsSDK.Components;
+using Esri.ArcGISMapsSDK.Utils;
+using TMPro;
+using UnityEngine;
+
+namespace Esri.ArcGISMapsSDK.Samples.Components
+{
+    [DisallowMultipleComponent]
+    [ExecuteAlways]
+    public class ArcGISAttributionDisplay : MonoBehaviour
+    {
+        private TextMeshPro attributionText;
+
+        private ArcGISMapComponent mapComponent;
+
+        private void OnEnable()
+        {
+            attributionText = GetComponent<TextMeshPro>();
+
+            if (attributionText == null)
+            {
+                return;
+            }
+
+            mapComponent = FindFirstObjectByType<ArcGISMapComponent>();
+
+            if (!mapComponent || !mapComponent.View)
+            {
+                Debug.LogError("Unable to find a parent ArcGISMapComponent.");
+
+                enabled = false;
+                return;
+            }
+
+            SetAttributionText(mapComponent.View.AttributionText);
+
+            mapComponent.View.AttributionChanged += () =>
+            {
+                SetAttributionText(mapComponent.View.AttributionText);
+            };
+        }
+
+        private void OnDisable()
+        {
+            if (mapComponent && mapComponent.View)
+            {
+                mapComponent.View.AttributionChanged = null;
+            }
+
+            SetAttributionText(string.Empty);
+        }
+
+        private void SetAttributionText(string text)
+        {
+            if (attributionText == null)
+            {
+                return;
+            }
+
+            ArcGISMainThreadScheduler.Instance().Schedule(() =>
+            {
+                attributionText.text = text;
+            });
+        }
+    }
+}
