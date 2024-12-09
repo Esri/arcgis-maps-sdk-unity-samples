f0ecf765add6b01dfa1b7878ea43b91e590232c4
Mason Gaw <mgaw@esri.com>
2024-12-09 15:06:19 -0800

    Rotate Map Option

--- sample_project/Assets/SampleViewer/Samples/OverviewMap/Scripts/LocationMarker.cs
+++ sample_project/Assets/SampleViewer/Samples/OverviewMap/Scripts/LocationMarker.cs
@@ -9,6 +9,7 @@ using Esri.ArcGISMapsSDK.Samples.Components;
 using Esri.ArcGISMapsSDK.Utils.GeoCoord;
 using Esri.GameEngine.Geometry;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LocationMarker : MonoBehaviour
 {
@@ -18,7 +19,10 @@ public class LocationMarker : MonoBehaviour
     private ArcGISLocationComponent cameraLocationComponent;
     private ArcGISLocationComponent locationComponent;
     private ArcGISMapComponent mapComponent;
+    [SerializeField] private GameObject northMarker;
     private float northOffset = 225.0f;
+    [SerializeField] private RawImage overviewMap;
+    [SerializeField] private Toggle rotationToggle;
 
     private void Awake()
     {
@@ -26,20 +30,44 @@ public class LocationMarker : MonoBehaviour
         cameraLocationComponent = cameraController.GetComponent<ArcGISLocationComponent>();
         locationComponent = GetComponent<ArcGISLocationComponent>();
         mapComponent = GetComponentInParent<ArcGISMapComponent>();
+        northMarker.SetActive(rotationToggle.isOn);
+        
+        rotationToggle.onValueChanged.AddListener(delegate
+        {
+            northMarker.SetActive(rotationToggle.isOn);
+        });
     }
 
     private void Update()
     {
-        cameraComponent.GetComponent<ArcGISLocationComponent>().Position = new ArcGISPoint(
-            cameraLocationComponent.Position.X,
-            cameraLocationComponent.Position.Y,
-            cameraComponent.GetComponent<ArcGISLocationComponent>().Position.Z, mapComponent.OriginPosition.SpatialReference);
-        locationComponent.Position = new ArcGISPoint(
-            cameraLocationComponent.Position.X,
-            cameraLocationComponent.Position.Y, locationComponent.Position.Z,
-            mapComponent.OriginPosition.SpatialReference);
-        locationComponent.Rotation =
-            new ArcGISRotation(cameraLocationComponent.Rotation.Heading + northOffset,
-                locationComponent.Rotation.Pitch, locationComponent.Rotation.Roll);
+        if (!rotationToggle.isOn)
+        {
+            cameraComponent.GetComponent<ArcGISLocationComponent>().Position = new ArcGISPoint(
+                cameraLocationComponent.Position.X,
+                cameraLocationComponent.Position.Y,
+                cameraComponent.GetComponent<ArcGISLocationComponent>().Position.Z, mapComponent.OriginPosition.SpatialReference);
+            locationComponent.Position = new ArcGISPoint(
+                cameraLocationComponent.Position.X,
+                cameraLocationComponent.Position.Y, locationComponent.Position.Z,
+                mapComponent.OriginPosition.SpatialReference);
+            locationComponent.Rotation =
+                new ArcGISRotation(cameraLocationComponent.Rotation.Heading + northOffset,
+                    locationComponent.Rotation.Pitch, locationComponent.Rotation.Roll);   
+        }
+        else
+        {
+            locationComponent.Rotation =
+                new ArcGISRotation(cameraLocationComponent.Rotation.Heading + northOffset,
+                    locationComponent.Rotation.Pitch, locationComponent.Rotation.Roll);   
+            overviewMap.transform.localEulerAngles = new Vector3(0, 0, (float)cameraLocationComponent.Rotation.Heading);
+            cameraComponent.GetComponent<ArcGISLocationComponent>().Position = new ArcGISPoint(
+                cameraLocationComponent.Position.X,
+                cameraLocationComponent.Position.Y,
+                cameraComponent.GetComponent<ArcGISLocationComponent>().Position.Z, mapComponent.OriginPosition.SpatialReference);
+            locationComponent.Position = new ArcGISPoint(
+                cameraLocationComponent.Position.X,
+                cameraLocationComponent.Position.Y, locationComponent.Position.Z,
+                mapComponent.OriginPosition.SpatialReference);
+        }
     }
 }
