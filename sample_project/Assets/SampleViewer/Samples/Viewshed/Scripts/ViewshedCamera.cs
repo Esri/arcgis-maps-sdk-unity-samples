f8fb7cb1fb43cb80188e4bedf2a35d5a9a268138
Stephen Nauman <snauman@esri.com>
2024-12-13 18:13:47 -0800

    Change script name

---
+++ sample_project/Assets/SampleViewer/Samples/Viewshed/Scripts/ViewshedCamera.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ViewshedCamera : MonoBehaviour
+{
+    [SerializeField] private Camera viewshedCamera;
+    [SerializeField] private int pixelHeight = 2048;
+    [SerializeField] private int pixelWidth = 2048;
+
+    private RenderTexture depthTexture;
+
+    private void Start()
+    {
+        if (viewshedCamera == null)
+        {
+            viewshedCamera = GetComponent<Camera>();
+        }
+
+        CreateObserverDepthTexture();
+    }
+
+    void CreateObserverDepthTexture()
+    {
+        RenderTextureDescriptor desc = new RenderTextureDescriptor
+        {
+            width = pixelWidth,
+            height = pixelHeight,
+            graphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R32_SFloat,
+            depthBufferBits = 32,
+            depthStencilFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.D32_SFloat,
+            msaaSamples = 1,
+            dimension = UnityEngine.Rendering.TextureDimension.Tex2D,
+            sRGB = false,
+            volumeDepth = 1
+        };
+
+        depthTexture = new RenderTexture(desc);
+        depthTexture.filterMode = FilterMode.Bilinear;
+
+        viewshedCamera.depthTextureMode = DepthTextureMode.Depth;
+        viewshedCamera.targetTexture = depthTexture;
+    }
+
+    private void Update()
+    {
+        //Shader.SetGlobalMatrix("_ObserverViewProjection", viewshedCamera.projectionMatrix * viewshedCamera.worldToCameraMatrix);
+        //Shader.SetGlobalFloat("_ViewshedDepthLimit", viewshedCamera.farClipPlane);
+    }
+}
