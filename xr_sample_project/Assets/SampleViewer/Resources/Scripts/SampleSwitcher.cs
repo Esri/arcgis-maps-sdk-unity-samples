1b7d41dc64b2e4820966248bc76e50e27ed3f4f4
mmgaw13 <moose.gaw@gmail.com>
2025-01-15 11:19:28 -0800

    Add Copy Right

--- xr_sample_project/Assets/SampleViewer/Resources/Scripts/SampleSwitcher.cs
+++ xr_sample_project/Assets/SampleViewer/Resources/Scripts/SampleSwitcher.cs
@@ -1,4 +1,9 @@
-using Esri.HPFramework;
+// Copyright 2025 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
