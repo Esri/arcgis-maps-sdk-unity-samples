87799bc8e1f12e74237509e91f1412db41241f49
Zack Allen <zallen@esri.com>
2025-01-16 13:37:59 -0800

    Fix copyright statement

--- sample_project/Assets/SampleViewer/Scripts/AutoImport.cs
+++ sample_project/Assets/SampleViewer/Scripts/AutoImport.cs
@@ -1,6 +1,7 @@
+// Copyright 2023 Esri.
 //
-//Written by Sam the intern
-//Reimports materials after render pipeline package is added/removed
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
 //
 #if UNITY_EDITOR
 using UnityEditor;
