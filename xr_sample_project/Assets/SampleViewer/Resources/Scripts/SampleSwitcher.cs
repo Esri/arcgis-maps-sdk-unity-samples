6952fca6b6d72ef5c6f178db0d7d28aea7ac900f
Mason Gaw <89485705+mmgaw113@users.noreply.github.com>
2025-01-16 10:38:13 -0800

    Feature: xr sample switcher (#172)
    * Add Furniture
    
    * Add Sample Viewer
    
    * Terrain
    
    * Fix Sample Switcher and Table top scenes
    
    * Add Copy Right
    
    * Update xr_sample_project/Assets/SampleViewer/Resources/Scripts/SampleSwitcher.cs
    
    Co-authored-by: Zack Allen <zallen@esri.com>
    
    * change font
    
    * Update terrain
    
    * Remove Terrain
    
    * Add terrain, update gitattribute rules for .asset type
    
    * Add terrain layers
    
    * Mark terrain as git lfs
    
    * Update sample viewer
    
    * Re-add Terrain
    
    ---------
    
    Co-authored-by: Zack Allen <zallen@esri.com>

---
+++ xr_sample_project/Assets/SampleViewer/Resources/Scripts/SampleSwitcher.cs
@@ -0,0 +1,48 @@
+// Copyright 2025 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+public class SampleSwitcher : MonoBehaviour
+{
+    [SerializeField] private bool isSampleViewer;
+
+    [SerializeField] private Button homeButton;
+    [SerializeField] private Button vrSceneButton;
+    [SerializeField] private Button xrTableTopSceneButton;
+
+
+    public void ChangeScene(string NextScene)
+    {
+        SceneManager.LoadSceneAsync(NextScene);
+    }
+
+    private void Start()
+    {
+        if (!isSampleViewer)
+        {
+            homeButton.onClick.AddListener(delegate
+            {
+                ChangeScene("SampleViewer");
+            });
+
+            return;
+        }
+
+        vrSceneButton.onClick.AddListener(delegate
+        {
+            ChangeScene("VR-Sample");
+        });
+
+        xrTableTopSceneButton.onClick.AddListener(delegate
+        {
+            ChangeScene("XRTableTop");
+        });
+    }
+}
