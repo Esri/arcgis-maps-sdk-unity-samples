8bb09d78d49263d16df19cf3b763a34738547257
Mason Gaw <89485705+mmgaw113@users.noreply.github.com>
2025-02-10 08:53:59 -0800

    Feature: xr sample viewer (#177)
    * Feature: xr sample switcher (#172)
    
    * Add Furniture
    
    * Add Sample Viewer
    
    * Terrain
    
    * Fix Sample Switcher and Table top scenes
    
    * Add Copy Right
    
    * Update xr_sample_project/Assets/SampleViewer/Resources/Scripts/SampleSwitcher.cs
    
    Co-authored-by: Zack Allen <zallen@esri.com>
    
    * change font
    
    * Update terrain
    
    * Remove Terrain
    
    * Add terrain, update gitattribute rules for .asset type
    
    * Add terrain layers
    
    * Mark terrain as git lfs
    
    * Update sample viewer
    
    * Re-add Terrain
    
    ---------
    
    Co-authored-by: Zack Allen <zallen@esri.com>
    
    * Bug: update terrain (#173)
    
    * Add terrain
    
    * update terrain
    
    * Sample Viewr: XR table top updates (#174)
    
    * Add Furniture
    
    * Add Sample Viewer
    
    * Terrain
    
    * Fix Sample Switcher and Table top scenes
    
    * Add Copy Right
    
    * Split Table top into two levels
    
    * Add AR Table top to sample viewer
    
    * Update Table Top for Sample Switcher
    
    * Feature: update vr sample (#175)
    
    * Add Furniture
    
    * Add Sample Viewer
    
    * Terrain
    
    * Fix Sample Switcher and Table top scenes
    
    * Add Copy Right
    
    * Split Table top into two levels
    
    * Add AR Table top to sample viewer
    
    * Update Table Top for Sample Switcher
    
    * Update VR Sample
    
    * Change turn type
    
    * Update packages for assembly reference
    
    * Update VR Sample
    
    * Terrain
    
    * Fix Sample Switcher and Table top scenes
    
    * Add AR Table top to sample viewer
    
    * Update VR Sample
    
    * Change turn type
    
    * Update packages for assembly reference
    
    * Update VR Sample
    
    * Fix conflicts
    
    * Update RayCastLocation.cs
    
    * Update RayCastLocation.cs
    
    * Update Docs (#176)
    
    * Update Docs
    
    * Update README.md
    
    Co-authored-by: and12279 <aren@esri.com>
    
    ---------
    
    Co-authored-by: and12279 <aren@esri.com>
    
    * Address Feedback
    
    ---------
    
    Co-authored-by: Zack Allen <zallen@esri.com>
    Co-authored-by: and12279 <aren@esri.com>

---
+++ xr_sample_project/Assets/SampleViewer/Resources/Scripts/SampleSwitcher.cs
@@ -0,0 +1,54 @@
+// Copyright 2025 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+public class SampleSwitcher : MonoBehaviour
+{
+    [SerializeField] private bool isSampleViewer;
+
+    [SerializeField] private Button homeButton;
+    [SerializeField] private Button arTableTopSceneButton;
+    [SerializeField] private Button vrSceneButton;
+    [SerializeField] private Button vrTableTopSceneButton;
+
+
+    public void ChangeScene(string NextScene)
+    {
+        SceneManager.LoadSceneAsync(NextScene);
+    }
+
+    private void Start()
+    {
+        if (!isSampleViewer)
+        {
+            homeButton.onClick.AddListener(delegate
+            {
+                ChangeScene("SampleViewer");
+            });
+
+            return;
+        }
+
+        arTableTopSceneButton.onClick.AddListener(delegate
+        {
+            ChangeScene("ARTableTop");
+        });
+
+        vrSceneButton.onClick.AddListener(delegate
+        {
+            ChangeScene("VR-Sample");
+        });
+
+        vrTableTopSceneButton.onClick.AddListener(delegate
+        {
+            ChangeScene("VRTableTop");
+        });
+    }
+}
