8391735813c3eb7f0302a4b15733eea68b5d02ae
Samuel Cartledge <scartledge@esri.com>
2026-08-18 13:16:05 -0700

    Update AutoImport.cs

--- sample_project/Assets/SampleViewer/Scripts/AutoImport.cs
+++ sample_project/Assets/SampleViewer/Scripts/AutoImport.cs
@@ -45,21 +45,27 @@ public static class CallReImport
     static void reImport()
     {
         List<string> importPaths = new List<string>();
+        importPaths.Add("Assets/SampleViewer/Resources/Shaders");
+        importPaths.Add("Assets/SampleViewer/Samples/FeatureLayer/Shaders");
         importPaths.Add("Assets/SampleViewer/Samples");
-        importPaths.Add("Packages/com.esri.arcgis-maps-sdk/SDK/Resources/Shaders/Materials/URP");
-        importPaths.Add("Packages/com.esri.arcgis-maps-sdk/SDK/Resources/Shaders/Materials/HDRP");
         importPaths.Add("Assets/Samples");
+
+        var pipelineTypeName = GraphicsSettings.defaultRenderPipeline?.GetType().FullName;
+        var pipelineMaterialsPath = pipelineTypeName == "UnityEngine.Rendering.HighDefinition.HDRenderPipelineAsset"
+            ? "Packages/com.esri.arcgis-maps-sdk/SDK/Resources/Shaders/Materials/HDRP"
+            : "Packages/com.esri.arcgis-maps-sdk/SDK/Resources/Shaders/Materials/URP";
+        importPaths.Add(pipelineMaterialsPath);
 
         foreach (string path in importPaths)
         {
-            AssetDatabase.ImportAsset(path, ImportAssetOptions.ImportRecursive);
+            AssetDatabase.ImportAsset(path, ImportAssetOptions.ImportRecursive | ImportAssetOptions.ForceUpdate);
         }
     }
 
     [MenuItem("Tools/ArcGIS Maps SDK/Re-Import Samples Materials")]
     public static void ManualCall()
     {
-       reImport();
+        reImport();
     }
 }
 #endif
